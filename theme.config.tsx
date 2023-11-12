1c32bcc22c6bb9bf6a17e40f4265497d468bcd9a
Njoku Emmanuel <kalunjoku123@gmail.com>
2023-11-13 05:51:56 +0900

    chore: add darkmode

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Descent Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/Descent-Collective/",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/Descent-Collective/docs",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Nextra Docs Template",
   },
-}
+};
 
-export default config
+export default config;
